419ea4556338610985c845cd62654a52d15757cc
Harshita Raj <113916503+harshitaraj1008@users.noreply.github.com>
2024-04-18 17:35:27 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "harshita@gmail.com"
+var acoountPassword ="12354"
+accountCity ="Jaipur"
+
+//accountId = 2
+
+accountEmail = "hv@hc.com"
+accontPassword="5646315"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accontPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+ 
+let name=" harshita"
+let age = 23
+let isLoggedIn= false
